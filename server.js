28024bad0d9d5a96ba1366f7c7b99f4bc07b5d78
michellekenia <keniamsoaress@gmail.com>
2024-01-19 15:20:15 -0300

    criar-rota-post

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import {Usuario} from './Usuario.js'
 const PORT = 3000
 const server = express()
 
-// Banco de dados em memoria 
+//Habilitar express para receber JSON no corpo da request
+server.use(express.json())
+
+//Banco de dados em memoria 
 const bancoDados = [
     new Usuario("5f1f987d-6acc-4ff1-9aec-d50de8e1382c", 'Michelle', 31, 'Mulher'), 
     new Usuario("5c358a37-bc56-45ff-a6e4-20099b4e773e", 'Michelle K', 31, 'Mulher'), 
@@ -17,19 +20,20 @@ const bancoDados = [
 //contidos no banco de dados - array  
 server.get("/usuarios/buscar", function(request,response){
 const objetoResposta = {
-    'itens': database
+    'itens': bancoDados
 }
-
 //mensagem fornecida após realizar a busca
 response.status(200).json(objetoResposta)
 })
 
-//Rota que busca um elemento do banco de dados a partir de um id específico 
+//Rota que busca um usuario do banco de dados a partir de um id específico 
 server.get("/usuarios/buscar/:idUsuario", function(request, response){
-console.log("Esse id veio da request", request.params.idUsuario)   
+console.log("Esse id veio da request", request.params.idUsuario) 
+
 const usuarioEncontrado = bancoDados.findIndex((usuario) => usuario.id === request.params.idUsuario)
 console.log('Valor do findIndex', usuarioEncontrado)
 console.log('usuario encontrado', bancoDados[usuarioEncontrado]) 
+
 if (usuarioEncontrado < 0 ) {
     response.status(404).json({
         mensagem: "Usuário não encontrado"
@@ -38,6 +42,20 @@ if (usuarioEncontrado < 0 ) {
 
 response.status(200).json(bancoDados[usuarioEncontrado])
 })
+
+//Rota que cria um usuario novo e inclui no banco de dados  
+server.post("/usuarios/cadastrar", function(request, response){
+
+    const {id, name, idade, genero, created_at, updated_at} = request.body
+    console.log("Dados recebidos", id, name, idade, genero, created_at, updated_at)
+
+    const novoUsuario = {id, name, idade, genero, created_at, updated_at}
+    bancoDados.push(novoUsuario)
+    
+   response.status(201).json(novoUsuario)
+})
+
+
 
 
 server.listen(PORT, ()=> console.log("Servidor rodando com sucesso."))
